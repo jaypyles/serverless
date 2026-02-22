d9793d54a7b24bb877f55d42a30fabc6ea19e279
Jayden Pyles <jpylesbusiness@gmail.com>
2026-02-22 17:10:23 -0600

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-mkdir functions
-
 echo "$SERVER_CODE" >server.js
 
 echo "$USER_FUNC_CODE" | jq -r 'to_entries[] | "\(.key)|\(.value)"' | while IFS="|" read -r filename content; do
