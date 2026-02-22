38734f5c10e1281172665f50b5fe9d8684351f74
Jayden Pyles <jpylesbusiness@gmail.com>
2026-02-22 17:14:11 -0600

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,12 @@ set -e
 
 echo "$SERVER_CODE" >server.js
 
+mkdir -p functions
+
 echo "$USER_FUNC_CODE" | jq -r 'to_entries[] | "\(.key)|\(.value)"' | while IFS="|" read -r filename content; do
+  if ! echo "$filename" | grep -q "\.js$"; then
+    filename="$filename.js"
+  fi
   echo "$content" >"functions/$filename"
 done
 
